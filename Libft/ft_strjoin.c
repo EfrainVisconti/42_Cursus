afb510517b4fb87676035f58254339203829620b
Efrain Eduardo Visconti Vivas <eviscont@c3r1s6.42urduliz.com>
2023-09-29 15:47:25 +0200

    progreso libft

--- Libft/ft_strjoin.c
+++ Libft/ft_strjoin.c
@@ -6,13 +6,46 @@
 /*   By: eviscont <eviscont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 16:17:11 by eviscont          #+#    #+#             */
-/*   Updated: 2023/09/25 16:49:36 by eviscont         ###   ########.fr       */
+/*   Updated: 2023/09/28 11:04:53 by eviscont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+/*Reserva (con malloc(3)) y devuelve una nueva string, formada por la
+concatenación de ’s1’ y ’s2’.*/
+
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*rest;
-	
-	
+	char			*res;
+	unsigned int	i;
+	unsigned int	j;
+
+	res = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	i = 0;
+	j = 0;
+	if (res == NULL)
+		return (0);
+	while (i < ft_strlen(s1))
+	{
+		res[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		res[i] = s2[j];
+		i++;
+		j++;
+	}
+	res[i] = '\0';
+	return (res);
 }
+
+// int	main(void)
+// {
+// 	char	s1[] = "";
+// 	char	s2[] = "mundo";
+
+// 	printf("nueva: %s",ft_strjoin(s1, s2));
+// 	return (0);
+// }
